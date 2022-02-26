066639075250a306d0f7b473653ba5cc5957d365
Timur Vergazov <205208@mail.ru>
2022-02-26 23:13:19 +0300

    Исправления

--- models/user.js
+++ models/user.js
@@ -6,14 +6,12 @@ const userSchema = new mongoose.Schema({
     required: true,
     minlength: 2,
     maxlength: 30,
-    default: "Лучше написать своё имя",
   },
   about: {
     type: String,
     required: true,
     minlength: 2,
     maxlength: 30,
-    default: "Я знаю, что вы сделали прошлым летом",
   },
   avatar: {
     type: String,
