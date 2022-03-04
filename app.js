6161306b000daea22abc6fcecf0e39b4f88b245e
Timur Vergazov <205208@mail.ru>
2022-03-04 12:24:47 +0300

    Исправления

--- app.js
+++ app.js
@@ -28,6 +28,11 @@ app.post(
   "/signup",
   celebrate({
     body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]+\.[a-zA-Z0-9()]+([-a-zA-Z0-9()@:%_\\+.~#?&/=#]*)/,
+      ),
       email: Joi.string().required().email(),
       password: Joi.string().required().min(8),
     }),
@@ -38,9 +43,6 @@ app.post(
   "/signin",
   celebrate({
     body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().uri(),
       email: Joi.string().required().email(),
       password: Joi.string().required(),
     }),
--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 /* eslint-disable consistent-return */
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+
 const User = require("../models/user");
 const NotFoundError = require("../errors/notFoundError");
 const ConflictError = require("../errors/conflictError");
